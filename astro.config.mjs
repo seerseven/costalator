79e8f5aa1cd4cc0f8f3e59cffb1a9b82046b42e2
seerseven <dev@seerseven.com>
2024-08-11 17:48:07 -0400

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		github({
-			// Replace 'username' and 'repository' with your GitHub username and repository name
-			deployTo: 'gh-pages', // Deploy to 'gh-pages' branch
-			username: 'your-github-username',
-			repository: 'your-repo-name',
-		}),
-	],
-	// Other configurations...
+	site: 'https://seerseven.github.io',
+	base: 'costalator',
 });
