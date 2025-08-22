7c2947f5fc011b602f250c60f0fd3ff78ae11e09
Seerseven <dev@seerseven.com>
2025-08-22 01:48:22 -0400

    remove jekyll + github deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://seerseven.github.io',
-	base: 'costalator',
+	site: 'https://seerseven.github.io/costalator',
+	base: '/costalator/',
 });
